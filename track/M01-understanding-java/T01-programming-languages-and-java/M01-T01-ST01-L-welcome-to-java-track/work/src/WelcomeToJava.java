44698d6158d0f3149d1bb8b575d348cde293e740
rajendra <rajendrarb318@gmail.com>
2026-07-30 12:40:32 +0530

    message

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,11 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+
+        System.out.println("welcome to java track");
+        System.out.println("i am already know how to build programming logic");
+        System.out.println("Now i am learning how to express that logic using java");
+
     }
 }
