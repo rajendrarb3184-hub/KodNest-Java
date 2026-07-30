3184e2116c0d0bb8c2db61a3c9540f2c25547cd2
rajendra <rajendrarb318@gmail.com>
2026-07-30 12:41:38 +0530

    added bye message

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -6,6 +6,7 @@ public class WelcomeToJava {
         System.out.println("welcome to java track");
         System.out.println("i am already know how to build programming logic");
         System.out.println("Now i am learning how to express that logic using java");
+        System.out.println("Byeee");
 
     }
 }
