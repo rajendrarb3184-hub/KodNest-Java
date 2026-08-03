d422366d89572a4050441ca4c73012c9b4d44c24
rajendra <rajendrarb318@gmail.com>
2026-08-03 19:56:16 +0530

    Added java practice program

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Practice01.java
@@ -0,0 +1,10 @@
+
+public class Practice01 {
+
+    public static void main(String[] args) {
+        System.err.println("---My Placement Goal ---");
+        System.err.println("Role: Software Developer");
+        System.err.println("Target: Get Placed");
+        System.err.println("Daily Practice: 8 Hours");
+    }
+}
