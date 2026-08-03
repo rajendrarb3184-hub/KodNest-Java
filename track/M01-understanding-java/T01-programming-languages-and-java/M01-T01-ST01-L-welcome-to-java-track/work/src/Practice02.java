2d301dea05a33277aa900da33722d49e6b95ad9b
rajendra <rajendrarb318@gmail.com>
2026-08-03 20:04:58 +0530

    Added java practice program

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Practice02.java
@@ -0,0 +1,12 @@
+
+public class Practice02 {
+
+    public static void main(String[] args) {
+        System.err.println("--------------------");
+        System.err.println("STUDENT PROFILE");
+        System.err.println("Name: Alexa");
+        System.err.println("Cours: java");
+        System.err.println("Level: Beginner");
+        System.err.println("--------------------");
+    }
+}
