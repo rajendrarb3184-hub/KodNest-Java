bae5fd7a36e47c72ddd9c6543e0c9f31ca77125d
rajendra <rajendrarb318@gmail.com>
2026-08-04 13:59:11 +0530

    added the practice program

---
+++ track/M02-IntroductionTo-java/PracticeLoopT01.java
@@ -0,0 +1,29 @@
+
+public class PracticeLoopT01 {
+
+    public static void main(String[] args) {
+        int num = -7;
+        int firstScore = 18;
+        int secondScore = 25;
+
+        if (num > 0) {
+            System.err.println("Number type: Positive");
+        } else if (num < 0) {
+            System.err.println("Number type: Negative");
+        } else {
+            System.err.println("Number type: Zero");
+        }
+
+        if (num % 2 == 0) {
+            System.err.println("Parity: Even");
+        } else {
+            System.err.println("Parity: Odd");
+        }
+
+        if (firstScore >= secondScore) {
+            System.err.println("Larger score: " + firstScore);
+        } else {
+            System.err.println("Larger score: " + secondScore);
+        }
+    }
+}
