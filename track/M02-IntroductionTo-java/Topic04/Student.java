67eb967eb120d0f074b9d949bb70a72197be9c3b
rajendra <rajendrarb318@gmail.com>
2026-08-10 13:14:53 +0530

    update changes

---
+++ track/M02-IntroductionTo-java/Topic04/Student.java
@@ -0,0 +1,15 @@
+
+class Student {
+
+    int roll;
+    String name;
+    double height;
+
+    void run() {
+        System.out.println("running");
+    }
+
+    void sleep() {
+        System.out.println("sleeping");
+    }
+}
---
+++ track/M02-IntroductionTo-java/Topic04/StudentApplication.java
@@ -0,0 +1,30 @@
+
+class StudentApplication {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+        s1.roll = 21;
+        s1.name = "rahul";
+        s1.height = 6.4;
+
+        System.out.println(s1.roll);
+        System.out.println(s1.name);
+        System.out.println(s1.height);
+
+        s1.sleep();
+        s1.run();
+
+        Student s2 = new Student();
+        s2.roll = 11;
+        s2.name = "rajendra";
+        s2.height = 8.4;
+
+        System.out.println(s2.roll);
+        System.out.println(s2.name);
+        System.out.println(s2.height);
+
+        s2.sleep();
+        s2.run();
+    }
+}
